9d7c5078e779bd40d5c2297377719a20199df9d5
Levon2014 <lev.2014@outlook.com>
2016-07-12 15:57:53 +0100

    Updated blink code

--- Levon Thompson/Blink/Blink.ino
+++ Levon Thompson/Blink/Blink.ino
@@ -1,16 +1,14 @@
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(9,OUTPUT);
+/* This sketch meets the acceptance criteria of this task: https://trello.com/c/N8ml0RLg/10-blink-an-led */
 
+void setup() {
+  pinMode(9,OUTPUT);
 }
+/* This is the setup of my blink program */
 
 void loop()  { 
-  
   digitalWrite(9, HIGH);
-  // put your main code here, to run repeatedly}:
-
-delay(100);
-digitalWrite(13,LOW);
-delay(1000);
-
+  delay(100);
+  digitalWrite(9,LOW);
+  delay(1000);
 }
+/* This is a code of my light turning on and off. */
